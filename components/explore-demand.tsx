187d1a77a10124ff3fd6a6ec08564151f74e06c7
Ankit-69k <ankitkumarray19@gmail.com>
2024-08-24 20:17:28 +0530

    added chart wrapper component

--- components/explore-demand.tsx
+++ components/explore-demand.tsx
@@ -4,28 +4,26 @@ import ExploreFormats from "./explore-formats";
 import VerticalBarChartComponent from "./chart/verticalbarchart/verticalbarchart";
 import { ChevronDownCircle } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import ChartWrapper from "./chart/chartWrapper";
 
 function ExploreDemand() {
   return (
-    <ExploreFormats
-      title={"Demand"}
-      description="Here are all the properties you have listed, switch to slaes or rental views for indepth insights of your listings and online presence."
+    <ChartWrapper
+      title="Demand"
+      description={
+        "Here are all the properties you have listed, switch to slaes or rental views for indepth insights of your listings and online presence."
+      }
     >
-      <Card className="p-0 border-0">
-        <CardHeader className="p-0">
-          <CardTitle className="text-base font-medium">Rentals</CardTitle>
-        </CardHeader>
-        <CardContent className="flex justify-between items-center w-full gap-4 p-0">
-          <VerticalBarChartComponent
-            dataKey={""}
-            value={100}
-            color={"#D1F6DB"}
-            selectedOption={""}
-          />
-          <ChevronDownCircle size={24} />
-        </CardContent>
-      </Card>
-    </ExploreFormats>
+      <div className="flex justify-between items-center w-full gap-4 p-0">
+        <VerticalBarChartComponent
+          dataKey={""}
+          value={100}
+          color={"#D1F6DB"}
+          selectedOption={""}
+        />
+        <ChevronDownCircle size={24} />
+      </div>
+    </ChartWrapper>
   );
 }
 
---
+++ public/svg/exceptions.tsx
@@ -0,0 +1,8 @@
+import { cn } from "@/lib/utils";
+import { ClassValue } from "clsx";
+import React from "react";
+
+interface Props {
+  className?: ClassValue;
+  color?: string;
+}
