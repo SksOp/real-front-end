b17dc4390f74cf0e45c4bf06b5d27d2e51dcc34c
Ankit-69k <ankitkumarray19@gmail.com>
2024-12-11 12:22:12 +0530

    added pwa configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,21 @@ export default function RootLayout({
       <Head>
         <meta charSet="utf-8" />
         <meta name="theme-color" content="#ff0000" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/assets/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/assets/favicon.svg" />
+        <link rel="shortcut icon" href="/assets/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content=" Keypilot" />
+        <link rel="manifest" href="/manifest.ts" />
       </Head>
       <body className={cn(inter.className, "select-none")}>
         <RootProvider>{children}</RootProvider>
